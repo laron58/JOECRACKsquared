8e6e26e817e698109f0c135271832cc1af0bf123
laron58 <141530865+laron58@users.noreply.github.com>
2024-05-16 09:37:16 -0500

    Update joecrack2.py
    + Comments
    + Excludes control characters from char list

--- joecrack2.py
+++ joecrack2.py
@@ -3,22 +3,27 @@ import time
 
 def intFuzz(name, max, length = 1, chrLim = 126):
     print(f"Fuzzing {name} block...")
+    #Gives time for the user to terminate fuzzing
     time.sleep(1)
     charList = []
+    #Exclues control codes from list
     for char in (chr(i) for i in range (32, chrLim + 1) if i not in range(127, 160)):
         charList.append(char)
     chars = "".join(charList)
-    #print(chars)
+    #Tests values {length} characters long and below
     for len in range(1, length + 1):
-        for comb in (itertools.product(chars, repeat = len)):
-            value = "".join(comb)
+        for tuple in (itertools.product(chars, repeat = len)):
+            value = "".join(tuple)
             try:
+                #Value must be int to pass
                 toInt = int(value)
-                if toInt > max or toInt < 1:
+                #Value must be in range to pass
+                if toInt not in range(1, max + 1):
                     raise ValueError
+                #If not caught by errors, pass
                 print(f"{value} passed {name} check")
             except ValueError:
-                #print(f"{value} failed point #{point}")
+                #Adds crashing value to specific list and full log
                 lists[f"{name}Crashes"].append(value)
                 logList.append(f"{value} crashed \"{name}\"")
 
@@ -29,14 +34,16 @@ def strFuzz(name, length = 1, chrLim = 126):
     for char in (chr(i) for i in range (32, chrLim + 1) if i not in range(127, 160)):
         charList.append(char)
     chars = "".join(charList)
-    #print(chars)
     for len in range(1, length + 1):
-        for comb in (itertools.product(chars, repeat = len)):
-            value = "".join(comb)
+        for tuple in (itertools.product(chars, repeat = len)):
+            value = "".join(tuple)
             try:
+                #Value must be str to pass
                 toStr = str(value)
-                if ord(toStr[0]) < 32 or ord(toStr[0]) > 126:
+                #Value must be within "printable" range
+                if ord(toStr[0]) not in range (32, 127):
                     raise ValueError
+                #Value must match allowed arguments
                 if name == "methodArg":
                     if toStr != "-b" and toStr != "-d":
                         raise ValueError
@@ -46,13 +53,14 @@ def strFuzz(name, length = 1, chrLim = 126):
                 elif name == "encryptModeArg":
                     if toStr != "-m" and toStr != "-s" and toStr != "-b":
                         raise ValueError
-                if name != "password" and name != "test":
+                if name != "password":
                     print(f"{value} passed {name} check")
             except ValueError:
                 #print(f"{value} failed point #{point}")
                 lists[f"{name}Crashes"].append(value)
                 logList.append(f"{value} crashed \"{name}\"")
 
+#Big list & dictionary
 logList = []
 lists = {
     "choiceCrashes":[],
@@ -65,19 +73,23 @@ lists = {
 }
 
 try:
+    #Max 2
     intFuzz("choice", 2)
+    #Max 3, chrLim = 591
     intFuzz("encryptMode", 3, chrLim = 591)
+    #Max 2, length 2
     intFuzz("method", 2, 2)
+    #Length 1, chrLim = 591
     strFuzz("password", 1, 591)
+    #Length 2
     strFuzz("methodArg", 2)
     strFuzz("choiceArg", 2)
     strFuzz("encryptModeArg", 2)
+#Condition to stop the fuzzer: Ctrl + C
 except KeyboardInterrupt:
     print("Fuzzing stopped.")
 
 print("Total crashes:", len(logList))
-#print(lists)
-
 with open("crashLog.txt", "w", encoding = "utf-8") as file:
     file.write("\n".join(logList))
 print("Crash log updated.")
@@ -85,28 +97,3 @@ for list in lists:
     with open(f"{list}.txt", "w", encoding = "utf-8") as file:
         file.write("\n".join(lists[list]))
 print("Crash lists updated.")
-    
-""" 
-choice = int(input("\n\n*******MENU*******\n\n1.Plaintext\n2.Encrpyted\nHow is the password stored: "))
-
-if choice == 2:
-    encryptMode = int(input("1.MD5\n2.SHA-256\n3.BCrypt (very slow)\nWhat encrpytion cracking mode: "))
-    
-method = int(input("1.BruteForce\n2.Dictionary\nWhat method: "))
-
-password = str(input("What is the target? "))
-
-if (choice == 1): #plaintext cracking
-    if (method == 1): #bruteforce
-        bruteForce(0,password)
-        
-    elif (method == 2): #dictionary
-        dictionaryAtk(0, password)
-                
-if (choice == 2): #hash cracking
-    if (method == 1): #bruteforce
-        bruteForce(encryptMode,password)
-                        
-    if (method == 2): #dictionary
-        dictionaryAtk(encryptMode,password)
-"""
